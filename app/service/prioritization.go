a4b0066a0fb2e48e4491b7652372c3ecc68615de
Harry Liu <magicoder20@gmail.com>
2021-10-10 11:45:24 -0700

    Draft execution service

--- app/service/prioritization.go
+++ app/service/prioritization.go
@@ -7,10 +7,19 @@ import (
 type Prioritization struct {
 }
 
+<<<<<<< HEAD
 func (p Prioritization) prioritizeTasks(inputTasks []entity.Task) []entity.Task {
 	return inputTasks
 }
 
 func NewPrioritization() Prioritization {
 	return Prioritization{}
+=======
+func (p Prioritization) PrioritizeTasks(inputTasks []entity.Task) []entity.Task {
+	return inputTasks
+}
+
+func (p Prioritization) SelectNeedAttention() *entity.Task {
+	panic("not implemented")
+>>>>>>> Draft execution service
 }
