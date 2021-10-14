dcdb3484c7eff593418c2102803dde085891b3d8
Magic Coder <magicoder20@gmail.com>
2021-10-14 01:49:17 -0700

    Add DB migration & seeding tools (#13)
    * Add DB migration & seeding tools
    
    * Remove invalid DB seed team member

---
+++ cli/db.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/teamyapp/teamy-backend/app/config"
+	"github.com/teamyapp/teamy-backend/app/repo/db"
+)
+
+func migrateDB(migrationDir string, steps int) error {
+	return withDB(func(sqlDB *sql.DB) error {
+		migrationURI := fmt.Sprintf("file://%s", migrationDir)
+
+		driver, err := postgres.WithInstance(sqlDB, &postgres.Config{})
+		if err != nil {
+			return err
+		}
+
+		m, err := migrate.NewWithDatabaseInstance(
+			migrationURI,
+			"postgres", driver)
+		if err != nil {
+			return err
+		}
+
+		err = m.Steps(steps)
+		if errors.Is(err, os.ErrNotExist) {
+			log.Println("no change in DB")
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		log.Println("finish DB migration")
+		return nil
+	})
+}
+
+func execSQL(sqlFileName string) error {
+	return withDB(func(sqlDB *sql.DB) error {
+		buf, err := ioutil.ReadFile(sqlFileName)
+		if err != nil {
+			return err
+		}
+		_, err = sqlDB.Exec(string(buf))
+		if err == nil {
+			log.Println("successfully seeded DB")
+		}
+		return err
+	})
+}
+
+func withDB(action func(sqlDB *sql.DB) error) error {
+	cfg, err := config.FromEnv()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return db.WithDB(cfg, action)
+}
