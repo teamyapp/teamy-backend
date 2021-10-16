d4fdd5341f7380f8721359cfef174e57483909e8
Magic Coder <magicoder20@gmail.com>
2021-10-16 11:53:33 -0700

    Generate SQL to create new DB (#16)
    * Generate SQL to create new DB
    
    * Clean up

--- cli/cmd.go
+++ cli/cmd.go
@@ -7,10 +7,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var dbName string
 var migrationSteps int
 var migrationDir string
 var migrationFileName string
-
 var seedFilePath string
 
 var rootCmd = &cobra.Command{
@@ -19,6 +19,14 @@ var rootCmd = &cobra.Command{
 
 var dbCmd = &cobra.Command{
 	Use: "db",
+}
+
+var newDBCmd = &cobra.Command{
+	Use:   "new",
+	Short: "Generate SQL to create new database",
+	Run: func(cmd *cobra.Command, args []string) {
+		newDB(dbName)
+	},
 }
 
 var migrateCmd = &cobra.Command{
@@ -74,6 +82,15 @@ func init() {
 		"app/repo/db/seed.sql",
 		"location of DB seed SQL")
 	dbCmd.AddCommand(seedCmd)
+
+	newDBCmd.Flags().StringVarP(
+		&dbName,
+		"name",
+		"n",
+		"",
+		"name of new DB")
+	newDBCmd.MarkFlagRequired("name")
+	dbCmd.AddCommand(newDBCmd)
 
 	rootCmd.AddCommand(dbCmd)
 }
