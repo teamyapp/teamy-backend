43e295b82790852b8656ef7f3897e70cc4f8e62d
Harry Liu <magicoder20@gmail.com>
2021-10-23 21:33:55 -0700

    Add core logic for personal status

--- app/service/execution.go
+++ app/service/execution.go
@@ -15,7 +15,34 @@ type Execution struct {
 }
 
 func (e Execution) GetPersonalStatus(userID oneEntity.ID) (entity.PersonalStatus, error) {
-	panic("not implemented")
+	if err := userID.IsValid(); err != nil {
+		log.Println(err)
+		return entity.PersonalStatus{}, err
+	}
+
+	upcomingTasks, err := e.taskRepo.FindTasksForUser(userID, entity.TaskStatusUpcoming)
+	if err != nil {
+		log.Println(err)
+		return entity.PersonalStatus{}, err
+	}
+
+	taskNeedAttention, err := e.taskRepo.FindTaskNeedAttentionForUser(userID)
+	if err != nil {
+		log.Println(err)
+		return entity.PersonalStatus{}, err
+	}
+
+	deliveredTasks, err := e.taskRepo.FindTasksForUser(userID, entity.TaskStatusDelivered)
+	if err != nil {
+		log.Println(err)
+		return entity.PersonalStatus{}, err
+	}
+
+	return entity.PersonalStatus{
+		TaskNeedAttention: taskNeedAttention,
+		UpcomingTasks:     e.prioritizationService.prioritizeTasks(upcomingTasks),
+		DeliveredTasks:    deliveredTasks,
+	}, nil
 }
 
 func (e Execution) GetActiveTeamStatus(userID oneEntity.ID) (entity.TeamStatus, error) {
