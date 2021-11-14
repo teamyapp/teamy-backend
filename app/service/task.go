dd3a0a2f5898cf6daabbe8f32d9d647ee2215bb1
yijiacc <76569613+yijia-cc@users.noreply.github.com>
2021-11-13 17:34:22 -0800

    Add CreateTask API to task service (#35)
    * Add CreateTask API
    
    * Add TODO for id generation

--- app/service/task.go
+++ app/service/task.go
@@ -22,7 +22,12 @@ func (t Task) FindTask(taskID oneEntity.ID) (entity.Task, error) {
 }
 
 func (t Task) CreateTask(task entity.Task) error {
-	panic("not implemented")
+	// TODO: generate unique id before adding to DB
+	err := t.taskRepo.CreateTask(task)
+	if err != nil {
+		log.Println(err)
+	}
+	return err
 }
 
 func (t Task) DeleteTask(taskID oneEntity.ID) error {
