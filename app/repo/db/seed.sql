978b25f41c4e4d5a63ca3e25760e0b40541e8856
yijia-cc <bestmissjia@gmail.com>
2021-11-16 22:25:26 -0800

    Change id to autoincrement in sql seed

--- app/repo/db/seed.sql
+++ app/repo/db/seed.sql
@@ -1,25 +1,24 @@
 -- Seed tables
 -- Users
-INSERT INTO "user" (id, name, profile_url) VALUES (1, 'Luke Walker', 'http://static.teamyapp.com/user/1/profile.png');
-INSERT INTO "user" (id, name, profile_url) VALUES (2, 'Mason Eyler', 'http://static.teamyapp.com/user/2/profile.png');
-INSERT INTO "user" (id, name, profile_url) VALUES (3, 'Milton Cutler', 'http://static.teamyapp.com/user/3/profile.png');
-INSERT INTO "user" (id, name, profile_url) VALUES (4, 'Milan Fender', 'http://static.teamyapp.com/user/4/profile.png');
-INSERT INTO "user" (id, name, profile_url) VALUES (5, 'Martin Hitler ', 'http://static.teamyapp.com/user/5/profile.png');
-INSERT INTO "user" (id, name, profile_url) VALUES (6, 'Myrnin Parker ', 'http://static.teamyapp.com/user/6/profile.png');
-INSERT INTO "user" (id, name, profile_url) VALUES (7, 'Jade Alpin ', 'http://static.teamyapp.com/user/7/profile.png');
-INSERT INTO "user" (id, name, profile_url) VALUES (8, 'Kyle Armster ', 'http://static.teamyapp.com/user/8/profile.png');
-
+INSERT INTO "user" (name, profile_url) VALUES ('Luke Walker', 'http://static.teamyapp.com/user/1/profile.png');
+INSERT INTO "user" (name, profile_url) VALUES ('Mason Eyler', 'http://static.teamyapp.com/user/2/profile.png');
+INSERT INTO "user" (name, profile_url) VALUES ('Milton Cutler', 'http://static.teamyapp.com/user/3/profile.png');
+INSERT INTO "user" (name, profile_url) VALUES ('Milan Fender', 'http://static.teamyapp.com/user/4/profile.png');
+INSERT INTO "user" (name, profile_url) VALUES ('Martin Hitler ', 'http://static.teamyapp.com/user/5/profile.png');
+INSERT INTO "user" (name, profile_url) VALUES ('Myrnin Parker ', 'http://static.teamyapp.com/user/6/profile.png');
+INSERT INTO "user" (name, profile_url) VALUES ('Jade Alpin ', 'http://static.teamyapp.com/user/7/profile.png');
+INSERT INTO "user" (name, profile_url) VALUES ('Kyle Armster ', 'http://static.teamyapp.com/user/8/profile.png');
 -- Tasks
-INSERT INTO task (id, goal, owner_user_id) VALUES (1, 'Draft need attention, upcoming, and delivered API', 1);
-INSERT INTO task (id, goal, due_at, owner_user_id) VALUES (2, 'Create/Delete/Edit task', '2021-10-31', 1);
-INSERT INTO task (id, goal, due_at, owner_user_id) VALUES (3, 'To implement the repository storing the graph structures', '2021-10-31', 1);
-INSERT INTO task (id, goal, owner_user_id) VALUES (4, 'Add unit tests to identity service core logic', 1);
-INSERT INTO task (id, goal) VALUES (5, 'Move Identity service code into official Identity service repo');
-INSERT INTO task (id, goal) VALUES (6, 'Productionise the core algorithm for graph based access control system');
-INSERT INTO task (id, goal, owner_user_id) VALUES (7, 'Refactor identity service prototype with  PubSub interface', 1);
-INSERT INTO task (id, goal, owner_user_id) VALUES (8, 'Write sample code for structural design pattern', 1);
-INSERT INTO task (id, goal) VALUES (9, 'Create a Typescript client lib for identity service');
-INSERT INTO task (id, goal) VALUES (10, 'Build a prototype of identity service with oauth working');
+INSERT INTO task (goal, owner_user_id) VALUES ('Draft need attention, upcoming, and delivered API', 1);
+INSERT INTO task (goal, due_at, owner_user_id) VALUES ('Create/Delete/Edit task', '2021-10-31', 1);
+INSERT INTO task (goal, due_at, owner_user_id) VALUES ('To implement the repository storing the graph structures', '2021-10-31', 1);
+INSERT INTO task (goal, owner_user_id) VALUES ('Add unit tests to identity service core logic', 1);
+INSERT INTO task (goal) VALUES ('Move Identity service code into official Identity service repo');
+INSERT INTO task (goal) VALUES ('Productionise the core algorithm for graph based access control system');
+INSERT INTO task (goal, owner_user_id) VALUES ('Refactor identity service prototype with  PubSub interface', 1);
+INSERT INTO task (goal, owner_user_id) VALUES ('Write sample code for structural design pattern', 1);
+INSERT INTO task (goal) VALUES ('Create a Typescript client lib for identity service');
+INSERT INTO task (goal) VALUES ('Build a prototype of identity service with oauth working');
 
 -- Task dependencies
 INSERT INTO task_dependency (need_before, need_after) VALUES (5, 2);
@@ -27,12 +26,12 @@ INSERT INTO task_dependency (need_before, need_after) VALUES (4, 3);
 INSERT INTO task_dependency (need_before, need_after) VALUES (1, 5);
 
 -- Teams
-INSERT INTO team (id, name, logo_url) VALUES (1, 'Teamy', 'http://static.teamyapp.com/team/1/logo.png');
-INSERT INTO team (id, name, logo_url) VALUES (2, 'Google', 'http://static.teamyapp.com/team/2/logo.png');
-INSERT INTO team (id, name, logo_url) VALUES (3, 'Apple', 'http://static.teamyapp.com/team/3/logo.png');
-INSERT INTO team (id, name, logo_url) VALUES (4, 'Amazon', 'http://static.teamyapp.com/team/4/logo.png');
-INSERT INTO team (id, name, logo_url) VALUES (5, 'Netflix', 'http://static.teamyapp.com/team/5/logo.png');
-INSERT INTO team (id, name, logo_url) VALUES (6, 'Microsoft', 'http://static.teamyapp.com/team/6/logo.png');
+INSERT INTO team (name, logo_url) VALUES ('Teamy', 'http://static.teamyapp.com/team/1/logo.png');
+INSERT INTO team (name, logo_url) VALUES ('Google', 'http://static.teamyapp.com/team/2/logo.png');
+INSERT INTO team (name, logo_url) VALUES ('Apple', 'http://static.teamyapp.com/team/3/logo.png');
+INSERT INTO team (name, logo_url) VALUES ('Amazon', 'http://static.teamyapp.com/team/4/logo.png');
+INSERT INTO team (name, logo_url) VALUES ('Netflix', 'http://static.teamyapp.com/team/5/logo.png');
+INSERT INTO team (name, logo_url) VALUES ('Microsoft', 'http://static.teamyapp.com/team/6/logo.png');
 
 -- Team members
 INSERT INTO team_member (team_id, user_id, need_attention_task_id) VALUES (1, 1, 5);
