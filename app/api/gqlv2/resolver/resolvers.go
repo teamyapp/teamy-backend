439af5afd482320e8647be1303c0fac1478c42e5
哲の王 <8919794+CreatCodeBuild@users.noreply.github.com>
2021-11-28 18:44:22 +0800

    GraphQL Version2 (#61)
    * pass ci
    
    * ignore json
    
    * remove mentioned & comment
    
    * Update app/api/gqlv2/resolver/data.go
    
    * Update app/api/gqlv2/resolver/data.go
    
    Co-authored-by: Magic Coder <magicoder20@gmail.com>

---
+++ app/api/gqlv2/resolver/resolvers.go
@@ -0,0 +1,71 @@
+package resolver
+
+import (
+	_ "embed"
+
+	"github.com/graph-gophers/graphql-go"
+)
+
+//go:embed graphiql.html
+var graphiql []byte
+
+func QraphiQL() []byte {
+	return graphiql
+}
+
+type Dependencies struct {
+	Data *Data
+}
+
+func NewDependencies() *Dependencies {
+	return &Dependencies{
+		Data: Read("./data.json"),
+	}
+}
+
+type Root struct {
+	Deps *Dependencies
+}
+
+//////////
+// User //
+//////////
+type User struct {
+	deps       *Dependencies
+	ID         graphql.ID
+	Name       string
+	ProfileUrl string
+}
+
+func (u User) Tasks() []Task {
+	tasks := u.deps.Data.FilterTasks(func(t Task) bool {
+		return t.CreatorID == u.ID
+	})
+	for i := range tasks {
+		tasks[i].deps = u.deps
+	}
+	return tasks
+}
+
+func (u User) TaskNeedAttention() *Task {
+	return &Task{}
+}
+
+func (u User) UpcomingTasks() []Task {
+	return nil
+}
+
+func (u User) DeliveredTasks() []Task {
+	return nil
+}
+
+func (t User) LifetimeEvents() []LifetimeEvent {
+	events := t.deps.Data.FilterLifetimeEvents(func(e LifetimeEvent) bool {
+		return e.ActorID == t.ID
+	})
+	for i := range events {
+		events[i].Deps = t.deps
+		events[i].EventType.dep = t.deps
+	}
+	return events
+}
---
+++ app/api/gqlv2/resolver/task.go
@@ -0,0 +1,42 @@
+package resolver
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/graph-gophers/graphql-go"
+)
+
+type Task struct {
+	// this is always injected by a parent level resolver which returns a Task, at runtime
+	deps *Dependencies
+	// these 4 can be fetched by a parent level resolver which returns a Task
+	// in terms of SQL, they could be part of the Task table
+	// other method resolvers could be "inner joins"
+	ID      graphql.ID
+	Goal    string
+	Context string
+	DueAt   *time.Time
+
+	// foreign keys
+	CreatorID graphql.ID
+}
+
+func (t Task) DependsOn() []Task { return []Task{} }
+func (t Task) Creator() (User, error) {
+	user, err := t.deps.Data.GetUser(t.CreatorID)
+	user.deps = t.deps
+	return user, err
+}
+func (t Task) Assignees() []User { return []User{} }
+func (t Task) LifetimeEvents() []LifetimeEvent {
+	events := t.deps.Data.FilterLifetimeEvents(func(e LifetimeEvent) bool {
+		fmt.Println(e.EventType.Creation.TaskID, t.ID)
+		return e.EventType.Creation.TaskID == t.ID
+	})
+	for i := range events {
+		events[i].Deps = t.deps
+		events[i].EventType.dep = t.deps
+	}
+	return events
+}
