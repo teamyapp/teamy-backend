9844c542768beb71cd168bde6828ca5bf66b8c94
CreatCodeBuild <8919794+CreatCodeBuild@users.noreply.github.com>
2021-11-28 10:10:09 +0000

    remove comment

--- app/api/gqlv2/resolver/resolvers.go
+++ app/api/gqlv2/resolver/resolvers.go
@@ -2,6 +2,7 @@ package resolver
 
 import (
 	_ "embed"
+	"fmt"
 
 	"github.com/graph-gophers/graphql-go"
 )
@@ -25,6 +26,29 @@ func NewDependencies() *Dependencies {
 
 type Root struct {
 	Deps *Dependencies
+}
+
+type Mentionable struct {
+	dep  *Dependencies
+	Type string
+	ID   graphql.ID
+}
+
+func (m Mentionable) ToUser() (*User, bool) {
+	fmt.Println("m.dep", m.dep)
+	u, err := m.dep.Data.GetUser(m.ID)
+	if err != nil || m.Type != "User" {
+		return nil, false
+	}
+	return &u, true
+}
+
+func (m Mentionable) ToTask() (*Task, bool) {
+	tasks := m.dep.Data.GetTasks([]graphql.ID{m.ID})
+	if len(tasks) == 0 || m.Type != "Task" {
+		return nil, false
+	}
+	return &tasks[0], true
 }
 
 //////////
