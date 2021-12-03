f02e044566924437267acfe6c8e521e1e168d94e
yijia-cc <bestmissjia@gmail.com>
2021-12-02 17:49:24 -0800

    Prioritize task by due date

--- app/service/prioritization.go
+++ app/service/prioritization.go
@@ -2,12 +2,24 @@ package service
 
 import (
 	"github.com/teamyapp/teamy-backend/app/entity"
+	"sort"
 )
 
 type Prioritization struct {
 }
 
 func (p Prioritization) PrioritizeTasks(inputTasks []entity.Task) []entity.Task {
+	sort.SliceStable(inputTasks, func(i, j int) bool {
+		if inputTasks[i].DueAt == nil && inputTasks[j].DueAt == nil {
+			return inputTasks[i].ID < inputTasks[j].ID
+		} else if inputTasks[i].DueAt == nil {
+			return false
+		} else if inputTasks[j].DueAt == nil {
+			return true
+		}
+		return inputTasks[i].DueAt.Before(*inputTasks[j].DueAt)
+	})
+
 	return inputTasks
 }
 
