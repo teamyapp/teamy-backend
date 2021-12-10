081ee557c625bcb7275d8a989053e82a5d47fd67
CreatCodeBuild <8919794+CreatCodeBuild@users.noreply.github.com>
2021-12-10 13:51:44 +0000

    team update type

--- app/entity/team.go
+++ app/entity/team.go
@@ -6,11 +6,23 @@ import (
 
 type Team struct {
 	entity.Entity
-	Name               string
-	IconURL            *string
-	CreatorID          entity.ID
-	MemberIDs          []entity.ID
-	Tasks              []entity.ID
+	Name      string
+	IconURL   *string
+	CreatorID entity.ID
+	MemberIDs OrderedSet_ID
+	Tasks     OrderedSet_ID
 	// user id -> task id
 	NeedAttentionTasks map[entity.ID]entity.ID
+}
+
+type OrderedSet_ID []entity.ID
+
+func (set OrderedSet_ID) Add(newID entity.ID) OrderedSet_ID {
+	for _, id := range set {
+		if id == newID {
+			return set
+		}
+	}
+	set = append(set, newID)
+	return set
 }
