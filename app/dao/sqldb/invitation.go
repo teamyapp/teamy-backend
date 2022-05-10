b656b5f5f4ebfc807cfa97cd1533d157c1d49eec
yijiacc <bestmissjia@gmail.com>
2022-05-10 00:53:24 -0700

    Address comments

---
+++ app/dao/sqldb/invitation.go
@@ -0,0 +1,109 @@
+package sqldb
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/teamyapp/teamy-backend/app/dao"
+	"github.com/teamyapp/teamy-backend/app/entityv2"
+)
+
+type Invitation struct {
+	db *sql.DB
+}
+
+var _ dao.Invitation = (*Invitation)(nil)
+
+func (i Invitation) FindInvitationByID(id uint64) (entityv2.Invitation, error) {
+	statement := `
+	SELECT
+		id,
+		sender_user_id,
+		receiver_first_name,
+		receiver_last_name,
+		receiver_user_id,
+		receiver_email,
+		team_id,
+		expire_at,
+		status,
+		code,
+		create_at
+	FROM invitation
+	WHERE id = $1;
+`
+	invitation := entityv2.Invitation{}
+	err := i.db.QueryRow(statement, id).
+		Scan(
+			&invitation.ID,
+			&invitation.SenderUserID,
+			&invitation.ReceiverFirstName,
+			&invitation.ReceiverLastName,
+			&invitation.ReceiverUserID,
+			&invitation.ReceiverEmail,
+			&invitation.TeamID,
+			&invitation.ExpireAt,
+			&invitation.Status,
+			&invitation.Code,
+			&invitation.CreatedAt,
+		)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return entityv2.Invitation{}, dao.ErrNotFound(fmt.Sprintf(
+			"invitation not found: id=%v",
+			id))
+	}
+
+	return invitation, err
+}
+
+func (i Invitation) FindInvitationsByTeamID(teamID uint64) ([]entityv2.Invitation, error) {
+	statement := `
+	SELECT
+		id,
+		sender_user_id,
+		receiver_first_name,
+		receiver_last_name,
+		receiver_user_id,
+		receiver_email,
+		team_id,
+		expire_at,
+		status,
+		code,
+		create_at
+	FROM invitation
+	WHERE team_id = $1;
+`
+	rows, err := i.db.Query(statement, teamID)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	invitations := make([]entityv2.Invitation, 0)
+	for rows.Next() {
+		invitation := entityv2.Invitation{}
+		err = rows.Scan(
+			&invitation.ID,
+			&invitation.SenderUserID,
+			&invitation.ReceiverFirstName,
+			&invitation.ReceiverLastName,
+			&invitation.ReceiverUserID,
+			&invitation.ReceiverEmail,
+			&invitation.TeamID,
+			&invitation.ExpireAt,
+			&invitation.Status,
+			&invitation.Code,
+			&invitation.CreatedAt,
+		)
+		if err != nil {
+			log.Println(err)
+		}
+
+		invitations = append(invitations, invitation)
+	}
+
+	return invitations, err
+}
