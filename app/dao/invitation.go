703382fd6fab05673e24b0bfa94bbd8bb4264541
yijiacc <76569613+yijia-cc@users.noreply.github.com>
2022-05-10 00:54:47 -0700

    Add Invitation dao (#143)
    * Add Invitation dao
    
    * Address comments

--- app/dao/invitation.go
+++ app/dao/invitation.go
@@ -1,8 +1,10 @@
 package dao
 
-import "github.com/teamyapp/teamy-backend/app/entityv2"
+import (
+	"github.com/teamyapp/teamy-backend/app/entityv2"
+)
 
 type Invitation interface {
 	FindInvitationByID(id uint64) (entityv2.Invitation, error)
-	FindInvitationsByTeamID(id uint64) ([]entityv2.Invitation, error)
+	FindInvitationsByTeamID(teamID uint64) ([]entityv2.Invitation, error)
 }
