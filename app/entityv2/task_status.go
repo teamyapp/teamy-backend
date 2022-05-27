3c985f3782f4b3375c9372509958557958269a09
Magicoder <magicoder20@gmail.com>
2022-05-26 23:47:03 -0700

    Update task status

--- app/entityv2/task_status.go
+++ app/entityv2/task_status.go
@@ -8,6 +8,14 @@ const (
 	TaskStatusInProgress TaskStatus = "IN_PROGRESS"
 	TaskStatusPaused     TaskStatus = "PAUSED"
 	TaskStatusAwaiting   TaskStatus = "AWAITING"
-	TaskStatusBlocked    TaskStatus = "BLOCKED"
-	TaskStatusDelivered  TaskStatus = "DELIVERED"
+	/*
+		TaskStatusBlocked
+		To unblock a task
+		1) Delete the task
+		2) Mark task awaiting on other task
+		3) Collect extra context
+		4) Received help (comment) from colleague
+	*/
+	TaskStatusBlocked   TaskStatus = "BLOCKED"
+	TaskStatusDelivered TaskStatus = "DELIVERED"
 )
