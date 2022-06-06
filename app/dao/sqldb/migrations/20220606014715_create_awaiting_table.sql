2ae37e2539ac0a4c3ab782ca95d1f3ce54237974
Magic Coder <magicoder20@gmail.com>
2022-06-06 01:51:36 -0700

    Create task awaiting table (#185)
    * Create awaiting table
    
    * Clean up

---
+++ app/dao/sqldb/migrations/20220606014715_create_awaiting_table.sql
@@ -0,0 +1,9 @@
+-- +migrate Up
+CREATE TABLE task_awaiting
+(
+	waiting_task_id BIGINT NOT NULL REFERENCES task (id) ON UPDATE CASCADE ON DELETE CASCADE,
+	wait_for_task_id BIGINT NOT NULL REFERENCES task (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- +migrate Down
+DROP TABLE task_awaiting;
