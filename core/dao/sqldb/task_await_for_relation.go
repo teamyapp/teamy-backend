2e07b0d639066292ada283073aa0c682a8ccc424
Magic Coder <magicoder20@gmail.com>
2022-06-13 02:38:14 -0700

    Update core/dao/sqldb/task_await_for_relation.go

--- core/dao/sqldb/task_await_for_relation.go
+++ core/dao/sqldb/task_await_for_relation.go
@@ -74,9 +74,9 @@ func (t TaskAwaitForRelation) CreateRelation(relation entity.TaskAwaitForRelatio
 	_, err := t.db.Exec(`
 	INSERT INTO task_await_for_relation
 	(
-	 	awaiting_task_id,
-	 	await_for_task_id,
-	 	created_at
+	    awaiting_task_id,
+	    await_for_task_id,
+	    created_at
 	)
 	VALUES ($1, $2, $3);
 `,
