9dd040a571f9e47d64d4616841a8661ce35762c3
Xianbo DUAN <duanxianbo@gmail.com>
2022-08-05 21:21:58 -0400

    add script for generating proto in win

---
+++ scripts/proto_gen_win.ps1
@@ -0,0 +1,11 @@
+$paths = @('core','apps')
+
+foreach ($path in $paths)
+{
+  $protoFiles = Get-ChildItem -Path $path -Filter *.proto -File -Recurse -Name
+
+  foreach ($protoFile in $protoFiles) {
+      protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative "$path\$protoFile"
+  }
+}
+
