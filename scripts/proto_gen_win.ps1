4daa4bb190cb4c639cce5a28dde144d83321c1da
Magic Coder <magicoder20@gmail.com>
2022-08-19 18:34:10 -0700

    Combine protogen scripts (#243)

--- scripts/proto_gen_win.ps1
+++
@@ -1,9 +0,0 @@
-$paths = @('core','apps')
-foreach ($path in $paths)
-{
-  $protoFiles = Get-ChildItem -Path $path -Filter *.proto -File -Recurse -Name
-  foreach ($protoFile in $protoFiles) {
-      protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative "$path\$protoFile"
-  }
-}
-
