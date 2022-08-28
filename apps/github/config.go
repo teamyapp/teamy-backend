0fb2fe0433e5f6c8d7ec1e2295e4f31f3038279e
Xianbo DUAN <duanxianbo@gmail.com>
2022-08-27 23:20:45 -0400

    add timeout + requestid middleware

--- apps/github/config.go
+++ apps/github/config.go
@@ -11,7 +11,6 @@ type AppConfig struct {
 	AppName                   string        `envconfig:"APPS_GITHUB_APP_NAME"`
 	InstallationValidDuration time.Duration `envconfig:"APPS_GITHUB_INSTALLATION_VALID_DURATION" default:"5m"`
 	WebhookSecret             string        `envconfig:"APPS_GITHUB_WEBHOOK_SECRET"`
-	RequestTimeout            time.Duration `envconfig:"REQUEST_TIMEOUT" default:"10s"`
 }
 
 func AppConfigFromEnv() (AppConfig, error) {
