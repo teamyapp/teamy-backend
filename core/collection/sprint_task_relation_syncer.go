fdfd0abcbb3ce6b3b28f6c70a55bbf29733264dc
duanxianbo <6356492+duanxianbo@users.noreply.github.com>
2022-08-28 14:16:29 -0400

    Realtime from sprint to backlog (#248)

--- core/collection/sprint_task_relation_syncer.go
+++ core/collection/sprint_task_relation_syncer.go
@@ -28,6 +28,24 @@ func (s SprintTaskRelationSyncer) CreateAndSyncSprintTaskRelation(sprintTaskRela
 	return nil
 }
 
+func (s SprintTaskRelationSyncer) DeleteAndSyncSprintTaskRelation(sprintID uint64, taskID uint64, OwningTeamID uint64) error {
+	err := s.sprintTaskRelationDao.DeleteSprintTaskRelation(sprintID, taskID)
+	if err != nil {
+		return err
+	}
+
+	s.realTimeStateSyncer.NotifyMutation(realtime.Mutation{
+		CollectionType: realtime.SprintTaskRelationCollectionType,
+		MutationType:   realtime.DeleteMutationType,
+		TeamIDs: []uint64{
+			OwningTeamID,
+		},
+		Payload: realtime.DeleteSprintTaskRelationPayload{SprintID: sprintID, TaskID: taskID},
+	})
+	return nil
+
+}
+
 func NewSprintTaskRelationSyncer(realTimeStateSyncer *realtime.StateSyncer, sprintTaskRelationDao dao.SprintTaskRelation) SprintTaskRelationSyncer {
 	return SprintTaskRelationSyncer{
 		realTimeStateSyncer:   realTimeStateSyncer,
