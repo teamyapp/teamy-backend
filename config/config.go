2335b84931c9abfc13e69415000b00c7e5e810f3
Magic Coder <magicoder20@gmail.com>
2022-09-25 10:00:31 -0700

    Add Teamy service account to access cloud APIs (#266)

--- config/config.go
+++ config/config.go
@@ -11,16 +11,17 @@ import (
 type Config struct {
 	sqldb.Config
 	config.Repo
-	CloudWebAPIExternalBaseURL string        `envconfig:"CLOUD_WEB_API_EXTERNAL_BASE_URL" default:"http://localhost:9011"`
-	CloudWebAPIBaseURL         string        `envconfig:"CLOUD_WEB_API_BASE_URL" default:"http://localhost:9011"`
-	CloudGRPCAPIHost           string        `envconfig:"CLOUD_GRPC_API_HOST" default:"localhost"`
-	CloudGRPCAPIPort           int           `envconfig:"CLOUD_GRPC_API_PORT" default:"9011"`
-	CloudGRPCAPIShouldEncrypt  bool          `envconfig:"CLOUD_GRPC_API_SHOULD_ENCRYPT" default:"false"`
-	AppsServiceAccountAPIToken string        `envconfig:"APPS_SERVICE_ACCOUNT_API_TOKEN" default:""`
-	TeamyAPIHost               string        `envconfig:"TEAMY_API_HOST" default:"localhost"`
-	TeamyAPIPort               int           `envconfig:"TEAMY_API_PORT" default:"9001"`
-	TeamyAPIShouldEncrypt      bool          `envconfig:"TEAMY_API_SHOULD_ENCRYPT" default:"false"`
-	RequestTimeout             time.Duration `envconfig:"REQUEST_TIMEOUT" default:"10s"`
+	CloudWebAPIExternalBaseURL  string        `envconfig:"CLOUD_WEB_API_EXTERNAL_BASE_URL" default:"http://localhost:9011"`
+	CloudWebAPIBaseURL          string        `envconfig:"CLOUD_WEB_API_BASE_URL" default:"http://localhost:9011"`
+	CloudGRPCAPIHost            string        `envconfig:"CLOUD_GRPC_API_HOST" default:"localhost"`
+	CloudGRPCAPIPort            int           `envconfig:"CLOUD_GRPC_API_PORT" default:"9011"`
+	CloudGRPCAPIShouldEncrypt   bool          `envconfig:"CLOUD_GRPC_API_SHOULD_ENCRYPT" default:"false"`
+	AppsServiceAccountAPIToken  string        `envconfig:"APPS_SERVICE_ACCOUNT_API_TOKEN" default:""`
+	TeamyServiceAccountAPIToken string        `envconfig:"TEAMY_SERVICE_ACCOUNT_API_TOKEN" default:""`
+	TeamyAPIHost                string        `envconfig:"TEAMY_API_HOST" default:"localhost"`
+	TeamyAPIPort                int           `envconfig:"TEAMY_API_PORT" default:"9001"`
+	TeamyAPIShouldEncrypt       bool          `envconfig:"TEAMY_API_SHOULD_ENCRYPT" default:"false"`
+	RequestTimeout              time.Duration `envconfig:"REQUEST_TIMEOUT" default:"10s"`
 }
 
 func FromEnv(dataCollector obs.DataCollector) (Config, error) {
