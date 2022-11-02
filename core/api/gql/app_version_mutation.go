4bdd087faf93a2c6ed98492ea9289d500f594f6b
Magicoder <magicoder20@gmail.com>
2022-11-01 23:59:48 -0700

    Add visible team APIs

--- core/api/gql/app_version_mutation.go
+++ core/api/gql/app_version_mutation.go
@@ -32,3 +32,19 @@ func (m Mutation) DeleteAppVersion(ct context.Context, args struct {
 }) (AppVersion, error) {
 	panic("implement me")
 }
+
+func (m Mutation) AddVisibleTeamToAppVersion(ct context.Context, args struct {
+	AppID         graphql.ID
+	VersionNumber int32
+	TeamID        uint64
+}) (AppVersion, error) {
+	panic("implement me")
+}
+
+func (m Mutation) RemoveVisibleTeamFromAppVersion(ct context.Context, args struct {
+	AppID         graphql.ID
+	VersionNumber int32
+	TeamID        uint64
+}) (AppVersion, error) {
+	panic("implement me")
+}
