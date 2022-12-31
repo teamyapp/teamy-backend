1c80dd7385c2fcaf8431c2f7b88b1459bfa2270c
duanxianbo <duanxianbo@gmail.com>
2022-12-30 21:27:13 -0500

    reorder interface func

--- core/realtime/mutation.go
+++ core/realtime/mutation.go
@@ -13,10 +13,10 @@ const (
 )
 
 type Mutation interface {
-	CleanUp(ct context.Context) error
 	GetID() uint64
 	Execute(ct context.Context) error
 	Undo() error
+	CleanUp(ct context.Context) error
 	GetClientNotifiers(ct context.Context) ([]*ClientNotifier, error)
 	ToMessage() MutationMessage
 }
