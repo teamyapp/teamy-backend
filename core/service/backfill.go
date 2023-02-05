70b63915881c325e7bad07b7cd78dda71996cc2c
szheng2207 <122830157+szheng2207@users.noreply.github.com>
2023-02-05 15:11:58 -0800

    Add skeleton for backfill APIs (#345)
    Co-authored-by: Magic Coder <magicoder20@gmail.com>

---
+++ core/service/backfill.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+
+	"github.com/teamyapp/cloud/app/api"
+	"github.com/teamyapp/cloud/libs/telemetry"
+)
+
+type Backfill struct {
+	dataCollector       telemetry.DataCollector
+	cloudClientRegistry *api.ClientRegistry
+}
+
+func (b Backfill) BackfillPullRequestLinks(ct context.Context, teamID string) error {
+        panic("implement me")
+	return nil
+}
+
+func (b Backfill) BackfillParticipantsBandwidth(ct context.Context, teamID string, sprintID string) error {
+        panic("implement me")
+	return nil
+}
+
+func NewBackfill(
+	dataCollector telemetry.DataCollector,
+	cloudClientRegistry *api.ClientRegistry,
+) Backfill {
+	return Backfill{
+		dataCollector:       dataCollector,
+		cloudClientRegistry: cloudClientRegistry,
+	}
+}
