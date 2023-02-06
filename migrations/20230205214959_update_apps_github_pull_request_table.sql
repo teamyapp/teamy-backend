ad3a25ab4826dba29e6ad887a26eb8b1f8e8bc41
szheng2207 <122830157+szheng2207@users.noreply.github.com>
2023-02-05 23:25:57 -0800

    Update migrations/20230205214959_update_apps_github_pull_request_table.sql
    Co-authored-by: Magic Coder <magicoder20@gmail.com>

--- migrations/20230205214959_update_apps_github_pull_request_table.sql
+++ migrations/20230205214959_update_apps_github_pull_request_table.sql
@@ -7,7 +7,7 @@ ALTER TABLE apps_github_pull_request
 
 -- +migrate Down
 ALTER TABLE apps_github_pull_request
-DROP COLUMN github_pull_request_number,
-	DROP COLUMN github_pull_request_url,
+    DROP COLUMN github_repository_owner,
 	DROP COLUMN github_repository_name,
-	DROP COLUMN github_repository_owner;
+    DROP COLUMN github_pull_request_number,
+	DROP COLUMN github_pull_request_url;
