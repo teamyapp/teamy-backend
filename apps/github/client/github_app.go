07a7bc67cb7f8414142aa0c84ff8f306d251b9ed
Magic Coder <magicoder20@gmail.com>
2023-02-15 23:58:31 -0800

    Rename Github App client (#361)

--- apps/github/client/github_app.go
+++ apps/github/client/github_app.go
@@ -12,7 +12,7 @@ import (
 	"github.com/teamyapp/cloud/libs/telemetry"
 )
 
-type App struct {
+type GithubApp struct {
 	dataCollector telemetry.DataCollector
 	appID         string
 	appPrivateKey *rsa.PrivateKey
@@ -21,22 +21,22 @@ type App struct {
 	installations map[string]*Installation
 }
 
-func (a *App) GetInstallation(installationID string) *Installation {
-	installation, ok := a.installations[installationID]
+func (g *GithubApp) GetInstallation(installationID string) *Installation {
+	installation, ok := g.installations[installationID]
 	if ok {
 		return installation
 	}
 
-	installation = newInstallation(a.dataCollector, a, installationID)
-	a.installations[installationID] = installation
+	installation = newInstallation(g.dataCollector, g, installationID)
+	g.installations[installationID] = installation
 	return installation
 }
 
-func (a *App) getOrRefreshAppJWT(ct context.Context) (string, *errs.Error) {
-	if a.jwt != nil && a.jwtExpireAt != nil {
+func (g *GithubApp) getOrRefreshAppJWT(ct context.Context) (string, *errs.Error) {
+	if g.jwt != nil && g.jwtExpireAt != nil {
 		// JWT token is not expired
-		if time.Now().UTC().Before(*a.jwtExpireAt) {
-			return *a.jwt, nil
+		if time.Now().UTC().Before(*g.jwtExpireAt) {
+			return *g.jwt, nil
 		}
 	}
 
@@ -51,7 +51,7 @@ func (a *App) getOrRefreshAppJWT(ct context.Context) (string, *errs.Error) {
 		IssuedAt: time.Now().Add(-1 * time.Minute).Unix(),
 		// no more than 10 minutes in the future based on CURRENT time (independent of IssuedAt)
 		ExpireAt: expireAt.Unix(),
-		Issuer:   a.appID,
+		Issuer:   g.appID,
 	}
 
 	buf, err := json.Marshal(payload)
@@ -60,7 +60,7 @@ func (a *App) getOrRefreshAppJWT(ct context.Context) (string, *errs.Error) {
 			Code:     errs.Serialization,
 			EmbedErr: err,
 		}
-		a.dataCollector.Logger.ErrorWithContext(ct, internalErr)
+		g.dataCollector.Logger.ErrorWithContext(ct, internalErr)
 		return "", internalErr
 	}
 
@@ -71,28 +71,28 @@ func (a *App) getOrRefreshAppJWT(ct context.Context) (string, *errs.Error) {
 			Code:     errs.Deserialization,
 			EmbedErr: err,
 		}
-		a.dataCollector.Logger.ErrorWithContext(ct, internalErr)
+		g.dataCollector.Logger.ErrorWithContext(ct, internalErr)
 		return "", internalErr
 	}
 
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims(payloadMap))
-	signedStr, err := jwtToken.SignedString(a.appPrivateKey)
+	signedStr, err := jwtToken.SignedString(g.appPrivateKey)
 	if err != nil {
 		internalErr := &errs.Error{
 			Code:     errs.Unknown,
 			EmbedErr: err,
 		}
-		a.dataCollector.Logger.ErrorWithContext(ct, internalErr)
+		g.dataCollector.Logger.ErrorWithContext(ct, internalErr)
 		return "", internalErr
 	}
 
-	a.jwt = &signedStr
-	a.jwtExpireAt = &expireAt
-	a.dataCollector.Logger.InfoWithContext(ct, fmt.Sprintf("Refreshed app JWT token expiring at %v", a.jwtExpireAt))
+	g.jwt = &signedStr
+	g.jwtExpireAt = &expireAt
+	g.dataCollector.Logger.InfoWithContext(ct, fmt.Sprintf("Refreshed app JWT token expiring at %v", g.jwtExpireAt))
 	return signedStr, nil
 }
 
-func NewApp(dataCollector telemetry.DataCollector, appID string, privateKeyPEM []byte) (*App, *errs.Error) {
+func NewGithubApp(dataCollector telemetry.DataCollector, appID string, privateKeyPEM []byte) (*GithubApp, *errs.Error) {
 	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privateKeyPEM)
 	if err != nil {
 		internalErr := &errs.Error{
@@ -103,7 +103,7 @@ func NewApp(dataCollector telemetry.DataCollector, appID string, privateKeyPEM [
 		return nil, internalErr
 	}
 
-	return &App{
+	return &GithubApp{
 		appID:         appID,
 		appPrivateKey: privateKey,
 		installations: map[string]*Installation{},
