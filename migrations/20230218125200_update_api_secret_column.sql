c11a07cffda062e1d397871a3e9a3a39f772b503
Junoth <36186063+Junoth@users.noreply.github.com>
2023-02-23 23:49:10 -0800

    Apply suggestions from code review
    Co-authored-by: Magic Coder <magicoder20@gmail.com>

--- migrations/20230218125200_update_api_secret_column.sql
+++ migrations/20230218125200_update_api_secret_column.sql
@@ -1,13 +1,13 @@
 -- +migrate Up
 ALTER TABLE app
 	DROP COLUMN apisecret,
-    DROP COLUMN app_name,
+        DROP COLUMN app_name,
     ADD COLUMN api_secret VARCHAR(128) NOT NULL,
     ADD COLUMN name VARCHAR(100) NOT NULL;
 
 -- +migrate Down
 ALTER TABLE app
     DROP COLUMN name,
-	DROP COLUMN api_secret,
+    DROP COLUMN api_secret,
     ADD COLUMN app_name VARCHAR(100) NOT NULL,
     ADD COLUMN apisecret VARCHAR(128) NOT NULL;
