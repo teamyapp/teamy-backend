747e97897f8ce34e807af129c41c463b967798dc
Shawn Zheng <szheng2207@gmail.com>
2023-02-28 00:12:24 -0800

    Add new column and update index for apps_github_app_installation table

--- migrations/20230227224042_update_apps_github_app_installation_table.sql
+++ migrations/20230227224042_update_apps_github_app_installation_table.sql
@@ -1,10 +1,10 @@
 -- +migrate Up
 ALTER TABLE apps_github_app_installation
 	ADD github_organization_id BIGINT;
-CREATE INDEX idx_github_organization_id
-    ON apps_github_app_installation (github_organization_id);
-CREATE INDEX idx_team_id
-    ON apps_github_app_installation (team_id);
+CREATE INDEX idx_apps_github_app_installation_github_organization_id
+	ON apps_github_app_installation (github_organization_id);
+CREATE INDEX idx_apps_github_app_installation_team_id
+	ON apps_github_app_installation (team_id);
 ALTER TABLE apps_github_app_installation
 	DROP CONSTRAINT apps_github_app_installation_pkey;
 ALTER TABLE apps_github_app_installation
@@ -12,9 +12,10 @@ ALTER TABLE apps_github_app_installation
 
 -- +migrate Down
 ALTER TABLE apps_github_app_installation
-	DROP COLUMN github_organization_id;
-DROP INDEX idx_team_id;
-ALTER TABLE apps_github_app_installation
 	DROP CONSTRAINT apps_github_app_installation_pkey;
 ALTER TABLE apps_github_app_installation
 	ADD PRIMARY KEY (id, team_id);
+DROP INDEX idx_apps_github_app_installation_team_id;
+DROP INDEX idx_apps_github_app_installation_github_organization_id;
+ALTER TABLE apps_github_app_installation
+	DROP COLUMN github_organization_id;
