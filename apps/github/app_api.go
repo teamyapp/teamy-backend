4ff5d08049e2eb21314d94cdf9e8fe70856b4d69
szheng2207 <122830157+szheng2207@users.noreply.github.com>
2023-03-10 22:59:30 -0800

    Update apps/github/app_api.go
    Co-authored-by: Magic Coder <magicoder20@gmail.com>

--- apps/github/app_api.go
+++ apps/github/app_api.go
@@ -814,7 +814,7 @@ func (a AppAPI) BackfillPullRequestMetadata(ct context.Context, empty *emptypb.E
 	}
 
 	pullRequests = collect.Filter(prs, func(pr entity.GithubPullRequest) bool {
-		return pr.RepositoryName == nil ||
+		return pullRequest.RepositoryName == nil ||
 			pr.RepositoryOwner == nil ||
 			pr.URL == nil ||
 			pr.Number == nil
