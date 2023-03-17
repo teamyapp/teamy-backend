a1efd8f4c15ae2974a07b2ec8aa80d0af10e5939
szheng2207 <122830157+szheng2207@users.noreply.github.com>
2023-03-16 23:04:40 -0700

    Update apps/github/client/rest_api.go
    Co-authored-by: Magic Coder <magicoder20@gmail.com>

--- apps/github/client/rest_api.go
+++ apps/github/client/rest_api.go
@@ -51,8 +51,6 @@ func (r RESTAPI) GetOrganizationByLogin(ct context.Context, installation *Instal
 	if res.StatusCode >= errs.HTTPClientErrors && res.StatusCode < errs.HTTPServerErrors {
 		if res.StatusCode == http.StatusNotFound {
 			internalErr = errs.NewError(errs.NotFound, "Not found")
-		} else {
-			internalErr = errs.NewError(errs.Unknown, "Unknown")
 		}
 		return entity.Organization{}, errs.NewError(errs.Unknown, "Unknown")
 	}
