b6462b1ec89c8d17d71cf4ca9d8206c1b168b1b9
duanxianbo <duanxianbo@gmail.com>
2023-03-18 22:31:06 -0400

    Make github_reviewer_node_id nullable

--- migrations/20230312153752_update_github_reviewer_id_column.sql
+++ migrations/20230312153752_update_github_reviewer_id_column.sql
@@ -1,7 +1,7 @@
 -- +migrate Up
 ALTER TABLE apps_github_code_review
     DROP CONSTRAINT apps_github_code_review_pkey,
-	ADD github_reviewer_node_id VARCHAR(20) DEFAULT "",
+	ADD github_reviewer_node_id VARCHAR(20),
     ADD PRIMARY KEY (github_pull_request_node_id, github_reviewer_node_id);
 
 -- +migrate Down
