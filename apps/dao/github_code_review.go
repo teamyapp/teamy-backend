2a7837c25a824c321d44349c88822435887ba46d
Magicoder <magicoder20@gmail.com>
2023-04-06 17:44:24 -0700

    Fix reviewer node ID bug

--- apps/dao/github_code_review.go
+++ apps/dao/github_code_review.go
@@ -8,7 +8,7 @@ import (
 )
 
 type GithubCodeReview interface {
-	FindCodeReviewByGithubReviewerID(ct context.Context, pullRequestNodeID string, githubReviewerID string) (entity.GithubCodeReview, *errs.Error)
+	FindCodeReviewByGithubReviewerID(ct context.Context, pullRequestNodeID string, githubReviewerNodeID string) (entity.GithubCodeReview, *errs.Error)
 	FindCodeReviewByInternalTaskID(ct context.Context, internalTaskID uint64) (entity.GithubCodeReview, *errs.Error)
 	CreateCodeReview(ct context.Context, codeReview entity.GithubCodeReview) *errs.Error
 	UpdateCodeReview(ct context.Context, codeReview entity.GithubCodeReview) *errs.Error
