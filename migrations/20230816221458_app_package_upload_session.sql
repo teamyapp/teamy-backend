12bc69ec42c51b64665cb5c914e760714a1072a2
duanxianbo <duanxianbo@gmail.com>
2023-08-19 23:13:43 -0400

    remove user id

--- migrations/20230816221458_app_package_upload_session.sql
+++ migrations/20230816221458_app_package_upload_session.sql
@@ -1,7 +1,6 @@
 -- +migrate Up
 CREATE TABLE app_package_upload_session (
 	app_id BIGINT NOT NULL REFERENCES app (id),
-    user_id BIGINT NOT NULL REFERENCES "user" (id),
     file_upload_session_id BIGINT NOT NULL,
 	version_number INT NOT NULL,
 	is_completed BOOLEAN NOT NULL DEFAULT FALSE,
