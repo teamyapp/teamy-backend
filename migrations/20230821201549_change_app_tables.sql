ad1734ea68d8827f572b4f0a02e3a37fd5b7e562
duanxianbo <duanxianbo@gmail.com>
2023-08-23 21:49:37 -0400

    revert db migrate order

--- migrations/20230821201549_change_app_tables.sql
+++ migrations/20230821201549_change_app_tables.sql
@@ -31,32 +31,37 @@ ALTER TABLE app_version
 
 -- +migrate Down
 ALTER TABLE app
-	RENAME COLUMN total_installations TO installation_count;
-ALTER TABLE app
-	DROP COLUMN managed_by_team_id;
-ALTER TABLE app
-	ADD api_secret VARCHAR(128) NOT NULL;
-ALTER TABLE app
-	ADD active_version_number INT;
-ALTER TABLE app
-	ADD creator_user_id BIGINT NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE;
+	ADD "description" TEXT NOT NULL;
 ALTER TABLE app
 	ADD "name" VARCHAR(100) NOT NULL;
 ALTER TABLE app
-	ADD "description" TEXT NOT NULL;
+	ADD creator_user_id BIGINT NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE app
+	ADD active_version_number INT;
+ALTER TABLE app
+	ADD api_secret VARCHAR(128) NOT NULL;
+ALTER TABLE app
+	DROP COLUMN managed_by_team_id;
+ALTER TABLE app
+	RENAME COLUMN total_installations TO installation_count;
 
-
-ALTER TABLE app_version 
-	RENAME COLUMN is_ready TO is_public;
 ALTER TABLE app_version
-	RENAME COLUMN "number" TO version_number;
-ALTER TABLE app_version
-	DROP COLUMN app_name;
-ALTER TABLE app_version
-	ADD icon_url VARCHAR(2048);
-ALTER TABLE app_version
-	ADD has_ui_extension BOOL NOT NULL DEFAULT FALSE;
+	ADD "changes" TEXT;
 ALTER TABLE app_version
 	ADD ui_extension_entrypoint_path VARCHAR(4096);
 ALTER TABLE app_version
-	ADD "changes" TEXT;
+	ADD has_ui_extension BOOL NOT NULL DEFAULT FALSE;
+ALTER TABLE app_version
+	ADD icon_url VARCHAR(2048);
+ALTER TABLE app_version
+	DROP COLUMN app_name;
+ALTER TABLE app_version
+	RENAME COLUMN "number" TO version_number;
+ALTER TABLE app_version 
+	RENAME COLUMN is_ready TO is_public;
+
+
+
+
+
+
