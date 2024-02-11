9833b77f7d3a519c12a26790a35755954a0836b7
xianbo duan <6356492+duanxianbo@users.noreply.github.com>
2024-02-10 22:51:43 -0500

    Apply suggestions from code review
    Co-authored-by: Magic Coder <magicoder20@gmail.com>

--- migrations/20240210193232_fix_group_and_app_tables.sql
+++ migrations/20240210193232_fix_group_and_app_tables.sql
@@ -3,19 +3,20 @@ ALTER TABLE filter_group
 DROP COLUMN count;
 
 ALTER TABLE app_version
-DROP CONSTRAINT IF EXISTS app_version_created_by_user_id_fkey;
+    DROP CONSTRAINT IF EXISTS app_version_created_by_user_id_fkey;
 
 ALTER TABLE app_version
-ADD CONSTRAINT app_version_created_by_user_id_fkey FOREIGN KEY (created_by_user_id) REFERENCES "user" (id) ON DELETE SET NULL;
+    ADD CONSTRAINT app_version_created_by_user_id_fkey FOREIGN KEY (created_by_user_id) 
+        REFERENCES "user" (id) ON DELETE SET NULL;
 
 -- +migrate Down
 
 ALTER TABLE app_version
-DROP CONSTRAINT IF EXISTS app_version_created_by_user_id_fkey;
+    DROP CONSTRAINT IF EXISTS app_version_created_by_user_id_fkey;
 
 ALTER TABLE app_version
-ADD CONSTRAINT app_version_created_by_user_id_fkey FOREIGN KEY (created_by_user_id) REFERENCES "user" (id) ON DELETE CASCADE;
+ADD CONSTRAINT app_version_created_by_user_id_fkey FOREIGN KEY (created_by_user_id) 
+    REFERENCES "user" (id) ON DELETE CASCADE;
 
 ALTER TABLE filter_group
     ADD count INT NOT NULL DEFAULT 0;
-
