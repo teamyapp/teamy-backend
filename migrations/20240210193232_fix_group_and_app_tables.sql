db7a0ba96d5457e24e908032b600fe04d0e83ec7
duanxianbo <duanxianbo@gmail.com>
2024-02-10 22:36:11 -0500

    Fix update and delete rollouty

---
+++ migrations/20240210193232_fix_group_and_app_tables.sql
@@ -0,0 +1,21 @@
+-- +migrate Up
+ALTER TABLE filter_group
+DROP COLUMN count;
+
+ALTER TABLE app_version
+DROP CONSTRAINT IF EXISTS app_version_created_by_user_id_fkey;
+
+ALTER TABLE app_version
+ADD CONSTRAINT app_version_created_by_user_id_fkey FOREIGN KEY (created_by_user_id) REFERENCES "user" (id) ON DELETE SET NULL;
+
+-- +migrate Down
+
+ALTER TABLE app_version
+DROP CONSTRAINT IF EXISTS app_version_created_by_user_id_fkey;
+
+ALTER TABLE app_version
+ADD CONSTRAINT app_version_created_by_user_id_fkey FOREIGN KEY (created_by_user_id) REFERENCES "user" (id) ON DELETE CASCADE;
+
+ALTER TABLE filter_group
+    ADD count INT NOT NULL DEFAULT 0;
+
