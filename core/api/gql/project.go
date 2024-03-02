60bad3657b56342a41db835287647ddff06f6cac
xianbo duan <6356492+duanxianbo@users.noreply.github.com>
2024-03-02 13:57:44 -0500

    Add Projects, Phase, story api contracts (#509)
    Co-authored-by: Magic Coder <magicoder20@gmail.com>

---
+++ core/api/gql/project.go
@@ -0,0 +1,100 @@
+package gql
+
+import (
+	"context"
+
+	"github.com/graph-gophers/graphql-go"
+	"github.com/teamyapp/cloud/libs/errs"
+	"github.com/teamyapp/teamy-backend/core/entity"
+)
+
+type Project struct {
+	deps    *Dependencies
+	project entity.Project
+}
+
+func (p Project) ID() graphql.ID {
+	return toGraphQLID(p.project.ID)
+}
+
+func (p Project) Name() string {
+	return p.project.Name
+}
+
+func (p Project) ExpectedStartAt() *graphql.Time {
+	return toGraphQLTimePtr(p.project.ExpectedStartAt)
+}
+
+func (p Project) ActualStartAt() *graphql.Time {
+	return toGraphQLTimePtr(p.project.ActualStartAt)
+}
+
+func (p Project) ExpectedEndAt() *graphql.Time {
+	return toGraphQLTimePtr(p.project.ExpectedEndAt)
+}
+
+func (p Project) ActualEndAt() *graphql.Time {
+	return toGraphQLTimePtr(p.project.ActualEndAt)
+}
+
+func (p Project) Creator(ct context.Context) (User, error) {
+	user, err := p.deps.userService.FindUserByID(ct, p.project.CreatorID)
+	if err != nil {
+		p.deps.logger.ErrorWithContext(ct, err)
+		return User{}, errs.ToResolverErr(err)
+	}
+
+	return newUser(p.deps, user), nil
+}
+
+func (p Project) CreatedAt() graphql.Time {
+	return toGraphQLTime(p.project.CreatedAt)
+}
+
+func (p Project) UpdatedAt() *graphql.Time {
+	return toGraphQLTimePtr(p.project.UpdatedAt)
+}
+
+func (p Project) Phases(ct context.Context) ([]Phase, error) {
+	panic("not implemented")
+}
+
+func (p Project) Stories(ct context.Context) ([]Story, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) CreateProject(ct context.Context, args struct {
+	Input struct {
+		Name            string
+		ExpectedStartAt *graphql.Time
+		ExpectedEndAt   *graphql.Time
+	}
+}) (Project, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) UpdateProject(ct context.Context, args struct {
+	ProjectID graphql.ID
+	Input     struct {
+		Name            string
+		ExpectedStartAt *graphql.Time
+		ActualStartAt   *graphql.Time
+		ExpectedEndAt   *graphql.Time
+		ActualEndAt     *graphql.Time
+	}
+}) (Project, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) DeleteProject(ct context.Context, args struct {
+	ProjectID graphql.ID
+}) (Project, error) {
+	panic("not implemented")
+}
+
+func newProject(deps *Dependencies, project entity.Project) Project {
+	return Project{
+		deps:    deps,
+		project: project,
+	}
+}
---
+++ core/api/gql/story.go
@@ -0,0 +1,123 @@
+package gql
+
+import (
+	"context"
+
+	"github.com/graph-gophers/graphql-go"
+	"github.com/teamyapp/cloud/libs/errs"
+	"github.com/teamyapp/teamy-backend/core/entity"
+)
+
+type Story struct {
+	deps  *Dependencies
+	story entity.Story
+}
+
+func (s Story) ID() graphql.ID {
+	return toGraphQLID(s.story.ID)
+}
+
+func (s Story) Name() string {
+	return s.story.Name
+}
+
+func (s Story) Creator(ct context.Context) (User, error) {
+	user, err := s.deps.userService.FindUserByID(ct, s.story.CreatorID)
+	if err != nil {
+		s.deps.logger.ErrorWithContext(ct, err)
+		return User{}, errs.ToResolverErr(err)
+	}
+
+	return newUser(s.deps, user), nil
+}
+
+func (s Story) Owner(ct context.Context) (User, error) {
+	user, err := s.deps.userService.FindUserByID(ct, s.story.OwnerID)
+	if err != nil {
+		s.deps.logger.ErrorWithContext(ct, err)
+		return User{}, errs.ToResolverErr(err)
+	}
+
+	return newUser(s.deps, user), nil
+}
+
+func (s Story) Status() entity.StoryStatus {
+	return s.story.Status
+}
+
+func (s Story) Priority() entity.Priority {
+	return s.story.Priority
+}
+
+func (s Story) CreatedAt() graphql.Time {
+	return toGraphQLTime(s.story.CreatedAt)
+}
+
+func (s Story) UpdatedAt() *graphql.Time {
+	return toGraphQLTimePtr(s.story.UpdatedAt)
+}
+
+func (s Story) Tasks(ct context.Context) ([]Task, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) CreateStory(ct context.Context, args struct {
+	ProjectID graphql.ID
+	Input     struct {
+		Name     string
+		OwnerID  graphql.ID
+		Priority entity.Priority
+	}
+}) (Story, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) UpdateStory(ct context.Context, args struct {
+	StoryID graphql.ID
+	Input   struct {
+		Name     string
+		OwnerID  graphql.ID
+		Status   entity.StoryStatus
+		Priority entity.Priority
+	}
+}) (Story, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) DeleteStory(ct context.Context, args struct {
+	StoryID graphql.ID
+}) (Story, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) AddTaskToStory(ct context.Context, args struct {
+	StoryID graphql.ID
+	TaskID  graphql.ID
+}) (Task, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) AddTasksToStory(ct context.Context, args struct {
+	StoryID graphql.ID
+	TaskIDs []graphql.ID
+}) (Story, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) RemoveTaskFromStory(ct context.Context, args struct {
+	StoryID graphql.ID
+	TaskID  graphql.ID
+}) (Task, error) {
+	panic("not implemented")
+}
+
+func (m Mutation) RemoveTasksFromStory(ct context.Context, args struct {
+	StoryID graphql.ID
+	TaskIDs []graphql.ID
+}) (Story, error) {
+	panic("not implemented")
+}
+
+func newStory(deps *Dependencies, story entity.Story) Story {
+	return Story{deps, story}
+}
