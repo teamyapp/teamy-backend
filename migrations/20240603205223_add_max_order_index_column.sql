fff78b13f520ee7e2aaa000d135d365b55072131
duanxianbo <duanxianbo@gmail.com>
2024-06-03 21:48:39 -0400

    add backfill

--- migrations/20240603205223_add_max_order_index_column.sql
+++ migrations/20240603205223_add_max_order_index_column.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-ALTER TABLE team ADD COLUMN max_group_order_index INT NOT NULL DEFAULT 0;
+ALTER TABLE team ADD COLUMN max_group_order_index INT NOT NULL DEFAULT -1;
 
 -- +migrate Down
 ALTER TABLE team DROP COLUMN max_group_order_index;
